9e0c9487c319d2e55cc1e7a75bbc49d71c0a1db1
Ly Nhu Ngoc <ngoccnlynhu@gmail.com>
2025-10-23 12:45:58 +0700

    Hoàn thành test token gửi về mail của sv3

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const upload = require("../middleware/uploadMiddleware");
+const { upload, processAvatar } = require("../middleware/uploadMiddleware");
 const { verifyToken } = require("../middleware/authMiddleware"); 
 
-router.put("/upload-avatar", verifyToken, upload.single("avatar"), authController.uploadAvatar);
+router.put("/upload-avatar", verifyToken, upload.single("avatar"), processAvatar, authController.uploadAvatar);
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(cors()); // ✅ Enable CORS cho frontend gọi được
 
-
 // Kết nối MongoDB
 mongoose.connect(
   process.env.MONGO_URI,
@@ -18,9 +17,13 @@ mongoose.connect(
 .then(() => console.log("✅ Kết nối MongoDB thành công"))
 .catch((err) => console.error("❌ Lỗi kết nối MongoDB:", err));
 
-// Import routes
+// ✅ Import routes
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
 const userRoutes = require('./routes/user');
-app.use('/users', userRoutes); // Mount route
+app.use('/users', userRoutes);
+
 const profileRoutes = require('./routes/Profile');
 app.use("/profile", profileRoutes);
 
