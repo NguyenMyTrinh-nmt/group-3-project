33c6bf8360bb271dcab5d903d6546f65002f55ce
Ly Nhu Ngoc <ngoccnlynhu@gmail.com>
2025-10-23 15:27:52 +0700

    hoan thanh hd5 sv3

--- backend/routes/logRoutes.js
+++ backend/routes/logRoutes.js
@@ -1,8 +1,13 @@
+// logRoutes.js
 const express = require('express');
 const router = express.Router();
 
 const logController = require('../controllers/logController');
 
-router.get('/', logController.getAllLogs);
+// 1. Lấy tất cả Logs: URL là /api/logs/all
+router.get('/all', logController.getLogs);
+
+// 2. Thêm Log: URL là /api/logs/add
+router.post('/add', logController.createLog);
 
 module.exports = router;
