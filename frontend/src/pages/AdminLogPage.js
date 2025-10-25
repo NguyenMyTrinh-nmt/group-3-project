5fab23deb4773a7b6eec3eaaf86ad52352327120
Lê Trần Khánh Quyên <letrankhanhquyenn@gmail.com>
2025-10-25 23:34:32 +0700

    Update AdminLogPage.js

--- frontend/src/pages/AdminLogPage.js
+++ frontend/src/pages/AdminLogPage.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useAuth } from "../auth/useAuth";
 import api from "../api/axios";
